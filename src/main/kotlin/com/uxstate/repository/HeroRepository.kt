8dae9ef8487015178e5f7d260d6e6152fd8549d4
Tonnie <vontonnie@gmail.com>
2022-08-15 12:44:16 +0300

    Declare page number to heroes list map

--- src/main/kotlin/com/uxstate/repository/HeroRepository.kt
+++ src/main/kotlin/com/uxstate/repository/HeroRepository.kt
@@ -1,2 +1,12 @@
 package com.uxstate.repository
 
+import com.uxstate.model.Hero
+
+interface HeroRepository {
+
+    //map page no to a list of heroes
+    val heroes:Map<Int,List<Hero>>
+
+    //individual pages reference
+    val page1:List<Hero>
+}
