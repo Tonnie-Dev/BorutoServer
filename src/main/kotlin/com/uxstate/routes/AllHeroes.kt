40729a81204547c59934828870c2b93676d84aff
Tonnie <vontonnie@gmail.com>
2022-08-15 16:21:58 +0300

    Handle NumberFormateExcepton for getAllHeroes Endpoint

--- src/main/kotlin/com/uxstate/routes/AllHeroes.kt
+++ src/main/kotlin/com/uxstate/routes/AllHeroes.kt
@@ -1,12 +1,29 @@
 package com.uxstate.routes
 
+import com.uxstate.model.ApiResponse
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.lang.NumberFormatException
 
-fun Route.getAllHeroes(){
+fun Route.getAllHeroes() {
 
-    get (path = "/boruto/heroes"){
+    get(path = "/boruto/heroes") {
 
-        
+        try {
+            //retrieve page query defaulting to 1 if null
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+        } catch (e: NumberFormatException) {
+
+
+            /*exception in Java that occurs when an attempt is made to
+            convert a string with an incorrect format to a numeric value*/
+            call.respond(message = ApiResponse(success = false,
+                    message = "Only Numbers Allowed"), status = HttpStatusCode.BadRequest)
+
+        }
+
     }
 
 }
