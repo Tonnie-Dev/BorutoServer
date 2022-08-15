0cbe028dd7e8e87eca93c5225ac61df052f04b4c
Tonnie <vontonnie@gmail.com>
2022-08-15 12:40:23 +0300

    Add Heroes repository file

--- src/main/kotlin/com/uxstate/model/ApiResponse.kt
+++ src/main/kotlin/com/uxstate/model/ApiResponse.kt
@@ -1,5 +1,15 @@
 package com.uxstate.model
 
+import kotlinx.serialization.Serializable
+
+
+/*Parcelable convert object to byte stream and pass
+the data between two processes in Android. Serialization
+converts POJO to JSON and be used across platforms.
+
+Serialization in this context enables us to send this
+object as a JSON or response*/
+@Serializable
 //This class will be returned to the client
 data class ApiResponse(
     val success: Boolean,
--- src/main/kotlin/com/uxstate/model/Hero.kt
+++ src/main/kotlin/com/uxstate/model/Hero.kt
@@ -1,6 +1,13 @@
 package com.uxstate.model
 
+import kotlinx.serialization.Serializable
+
 //we will not be returning this data class directly
+
+/*Parcelable converts object to byte stream and pass
+the data between two process in Android. Serialization
+converts POJO to JSON and be used across platforms*/
+@Serializable
 data class Hero(
     val id: Int,
     val name: String,
