21299ab649dc601ab3dc2e97e66900f4823f7b1b
Tonnie <vontonnie@gmail.com>
2022-08-15 16:43:05 +0300

    Handle IllegalArgumentExcepton for getAllHeroes Endpoint

--- src/main/kotlin/com/uxstate/routes/AllHeroes.kt
+++ src/main/kotlin/com/uxstate/routes/AllHeroes.kt
@@ -5,6 +5,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 fun Route.getAllHeroes() {
@@ -14,6 +15,10 @@ fun Route.getAllHeroes() {
         try {
             //retrieve page query defaulting to 1 if null
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5)
+            //respond
+
+            call.respond(message = page)
         } catch (e: NumberFormatException) {
 
 
@@ -21,6 +26,14 @@ fun Route.getAllHeroes() {
             convert a string with an incorrect format to a numeric value*/
             call.respond(message = ApiResponse(success = false,
                     message = "Only Numbers Allowed"), status = HttpStatusCode.BadRequest)
+
+        } catch (e: IllegalArgumentException) {
+
+
+            /*exception in Java that occurs when an attempt is made to
+            convert a string with an incorrect format to a numeric value*/
+            call.respond(message = ApiResponse(success = false,
+                    message = "Heroes not found"), status = HttpStatusCode.BadRequest)
 
         }
 
