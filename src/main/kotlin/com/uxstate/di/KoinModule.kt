66581a6520390e263c1dc23672ad093ccc540187
Tonnie <vontonnie@gmail.com>
2022-08-15 16:05:16 +0300

    Create Koin Injection module

--- src/main/kotlin/com/uxstate/di/KoinModule.kt
+++ src/main/kotlin/com/uxstate/di/KoinModule.kt
@@ -1,2 +1,18 @@
 package com.uxstate.di
 
+import com.uxstate.repository.HeroRepository
+import com.uxstate.repository.HeroRepositoryImpl
+import org.koin.dsl.module
+
+//module{} (from koin) specifies how to inject certain types e.g. repository
+val koinModule = module {
+
+    //specify how to specify a kind inside here using singleton for single instance
+
+    single<HeroRepository> {
+
+        //specify the implementation
+        HeroRepositoryImpl()
+    }
+
+}
--- src/main/kotlin/com/uxstate/plugins/Koin.kt
+++ src/main/kotlin/com/uxstate/plugins/Koin.kt
@@ -1,14 +1,21 @@
 package com.uxstate.plugins
 
+import com.uxstate.di.koinModule
+
 import io.ktor.server.application.*
+
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureKoin(  ){
 
+    //installing koin plugin
     install(Koin){
 
         //initialize logger
         slf4jLogger()
+
+        //call koin's module passing in a variable
+       modules(koinModule)
     }
 }
