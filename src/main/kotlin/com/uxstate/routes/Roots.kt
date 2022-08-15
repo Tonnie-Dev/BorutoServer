f7a6bfcb04c9275394a28a23e059497a36fb4d51
Tonnie <vontonnie@gmail.com>
2022-08-15 13:10:39 +0300

    Add Root endpoint on Route ext function

--- src/main/kotlin/com/uxstate/routes/Roots.kt
+++ src/main/kotlin/com/uxstate/routes/Roots.kt
@@ -1,8 +1,16 @@
 package com.uxstate.routes
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.root(){
+//root endpoint
 
-    
+    get(path = "/"){
+
+        call.respond(message = "Welcome to Boruto API", status = HttpStatusCode.OK)
+    }
+
 }
