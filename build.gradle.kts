bd7ab4e4e0ca7888eaeaa98434ad5879995e2e3f
Tonnie <vontonnie@gmail.com>
2022-08-16 17:40:07 +0300

    Add StatusPages plugin gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,10 @@ dependencies {
     //adds the standard Server and Date headers into each response.
     implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
 
+    //status pages
+
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
--- src/main/kotlin/com/uxstate/plugins/StatusPages.kt
+++ src/main/kotlin/com/uxstate/plugins/StatusPages.kt
@@ -1,10 +1,11 @@
 package com.uxstate.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
 
 fun Application.configureStatusPages(){
 
     //call install() ext fxn on Ktor's Application class
 
-    this.install(StatusP)
+    this.install(StatusPages)
 }
