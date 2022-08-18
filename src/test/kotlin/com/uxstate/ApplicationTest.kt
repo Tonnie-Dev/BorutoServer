b67b3690f82337a3f4fc01247cd8b97092d13e8d
Tonnie <vontonnie@gmail.com>
2022-08-18 20:53:55 +0300

    Correct Koin not started error

--- src/test/kotlin/com/uxstate/ApplicationTest.kt
+++ src/test/kotlin/com/uxstate/ApplicationTest.kt
@@ -1,23 +1,42 @@
 package com.uxstate
 
 import com.uxstate.model.ApiResponse
-import com.uxstate.plugins.configureRouting
+import com.uxstate.model.Hero
+import com.uxstate.plugins.configureKoin
 import com.uxstate.repository.HeroRepository
+import com.uxstate.repository.NEXT_PAGE_KEY
+import com.uxstate.repository.PREV_PAGE_KEY
 import io.ktor.client.call.*
-import io.ktor.http.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import io.ktor.util.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.koin.java.KoinJavaComponent.inject
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     //inject heroRepository to access heroes list for page one
-    private val repository:HeroRepository by inject(HeroRepository::class.java)
+    private val repository: HeroRepository by inject(HeroRepository::class.java)
+
+
+/*@BeforeTest
+fun setup() = testApplication {
+
+
+    application {
+        configureKoin()
+
+    }
+
+
+}*/
+
+
     /*@Test
     fun testRoot() = testApplication {
 
@@ -41,7 +60,7 @@ class ApplicationTest {
         assertEquals(expected = "\"Welcome to Boruto API\"", actual = response.bodyAsText())
     }
 
-    @OptIn(InternalAPI::class)
+
     @Test
     fun `access all heroes endpoint, assert correct information`() = testApplication {
 
@@ -49,13 +68,15 @@ class ApplicationTest {
             developmentMode = false
         }
 
+        //list of pages
+     //   val heroes = repository.page1
         val response = client.get("/boruto/heroes")
 
         //first check the returned status code is OK
 
         assertEquals(expected = HttpStatusCode.OK, actual = response.status)
 
-        val expected = ApiResponse(success =true,
+        val expected = ApiResponse(success = true,
                 message = "OK",
                 previousPage = null,
                 nextPage = 2,
@@ -64,8 +85,8 @@ class ApplicationTest {
         //response from the server
 
 
-      //val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
-     val actual = Json.decodeFromString<ApiResponse>(response.body())
+        //val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+        val actual = Json.decodeFromString<ApiResponse>(response.body())
 
         println("Actual isvv $actual")
         println("expected isvv $expected")
@@ -73,13 +94,64 @@ class ApplicationTest {
         assertEquals(expected = expected, actual = actual)
     }
 
-
-
     @Test
-    fun `access all heroes endpoint, query all pages, assert correct information`()=
-        testApplication {
+    fun `access all heroes endpoint, query all pages, assert correct information`() = testApplication {
 
-            val response = client.get("/boruto/heroes")
+            //range
+            val pages = 1..5
+
+
+          pages.forEach { page ->
+
+              val response = client.get("/boruto/heroes?page=${page}")
+
+              assertEquals(expected = HttpStatusCode.OK, actual = response.status)
+
+              val expected = ApiResponse(success = true,
+                      message = "OK",
+                      previousPage = calculatePageNumber(page)[PREV_PAGE_KEY],
+                      nextPage = calculatePageNumber(page)[NEXT_PAGE_KEY],
+                      heroes = listOf(repository.page1, repository.page2, repository.page3,
+                              repository.page4, repository.page5)[page -1])
+
+              val actual = Json.decodeFromString<ApiResponse>(response.body())
+
+              assertEquals(expected = expected, actual = actual)
+              
+            }
+
+
+        }
+
+
+
+    private fun calculatePageNumber(page:Int):Map<String, Int?>{
+
+        var nextPage:Int? = page
+        var prevPage:Int? = page
+
+        if (page in 1..4){
+
+            nextPage = page.plus(1)
+        }
+        if (page in 2..5){
+
+            prevPage = page.minus(1)
+        }
+
+        //no room to decrease next page
+        if (page==1){
+
+            prevPage = null
+        }
+
+        //no room to increase next page
+        if (page==5){
+            nextPage = null
+        }
+
+
+        return mapOf(PREV_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
 
     }
 }
