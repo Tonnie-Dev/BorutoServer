02bd3f9e864608c18626a97013af1142246658af
Tonnie <vontonnie@gmail.com>
2022-08-18 12:44:30 +0300

    Correct JSON end of file error

--- src/test/kotlin/com/uxstate/ApplicationTest.kt
+++ src/test/kotlin/com/uxstate/ApplicationTest.kt
@@ -1,16 +1,23 @@
 package com.uxstate
 
+import com.uxstate.model.ApiResponse
+import com.uxstate.plugins.configureRouting
 import com.uxstate.repository.HeroRepository
+import io.ktor.client.call.*
 import io.ktor.http.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import io.ktor.util.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
 
-    val repository:HeroRepository by inject(HeroRepository::class.java)
+    //inject heroRepository to access heroes list for page one
+    private val repository:HeroRepository by inject(HeroRepository::class.java)
     /*@Test
     fun testRoot() = testApplication {
 
@@ -34,14 +41,34 @@ class ApplicationTest {
         assertEquals(expected = "\"Welcome to Boruto API\"", actual = response.bodyAsText())
     }
 
+    @OptIn(InternalAPI::class)
     @Test
     fun `access all heroes endpoint, assert correct information`() = testApplication {
+
+        environment {
+            developmentMode = false
+        }
 
         val response = client.get("/boruto/heroes")
 
         //first check the returned status code is OK
 
-        assertEquals(expected = HttpStatusCode.OK, response.status)
+        assertEquals(expected = HttpStatusCode.OK, actual = response.status)
+
+        val expected = ApiResponse(success =true,
+                message = "OK",
+                previousPage = null,
+                nextPage = 2,
+                heroes = repository.page1)
+
+        //response from the server
+
+
+     // val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+     val actual = Json.decodeFromString<ApiResponse>(response.body())
+
+        println("Actual is $actual")
+        assertEquals(expected = expected, actual = actual)
     }
 
 
