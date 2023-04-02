85fc1ba1429194baf5c502f650a00028a43e6f60
Tonnie <vontonnie@gmail.com>
2023-04-02 13:17:28 +0300

    Implement searchHeroes() for HeroRepositoryImplAlt

--- src/main/kotlin/com/uxstate/repository/HeroRepositoryAlt.kt
+++ src/main/kotlin/com/uxstate/repository/HeroRepositoryAlt.kt
@@ -1,10 +1,11 @@
 package com.uxstate.repository
 
 import com.uxstate.model.ApiResponse
+import com.uxstate.model.Hero
 
 interface HeroRepositoryAlt {
 
-
+val heroes:List<Hero>
     suspend fun getAllHeroes(page:Int = 1, limit:Int = 4    )
     suspend fun searchHeroes(name:String?):ApiResponse
 }
