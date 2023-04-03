922e572850068a967c53b5bd217232f269a22def
Tonnie <vontonnie@gmail.com>
2023-04-03 07:58:12 +0300

    Inject HeroRepositoryAlt inside KoinModule

--- src/main/kotlin/com/uxstate/di/KoinModule.kt
+++ src/main/kotlin/com/uxstate/di/KoinModule.kt
@@ -1,19 +1,31 @@
 package com.uxstate.di
 
 import com.uxstate.repository.HeroRepository
+import com.uxstate.repository.HeroRepositoryAlt
 import com.uxstate.repository.HeroRepositoryImpl
+import com.uxstate.repository.HeroRepositoryImplAlt
 import org.koin.dsl.module
 
 //module{} (from koin) specifies how to inject certain types e.g. repository
 val koinModule = module {
 
-    //specify how to specify a kind inside here using singleton for
-    // single instance - across the Boruto server
+  /*  defines how to specify a kind inside here using singleton for
+    single instance - across the Boruto server*/
 
     single<HeroRepository> {
 
         //specify the implementation
         HeroRepositoryImpl()
+    }
+
+    /*
+    single -implies this is a singleton
+    HeroRepositoryAlt - this is the type
+    HeroRepositoryImplAlt - is the implementation
+    */
+    single <HeroRepositoryAlt>{
+
+        HeroRepositoryImplAlt()
     }
 
 }
---
+++ src/main/kotlin/com/uxstate/routes/AllHeroesEndpointAlt.kt
@@ -0,0 +1,61 @@
+package com.uxstate.routes
+
+import com.uxstate.model.ApiResponse
+import com.uxstate.repository.HeroRepository
+import com.uxstate.repository.HeroRepositoryAlt
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+fun Route.getAllHeroesAlt() {
+
+    //inject heroRepository - it will automatically find the impl from coin
+
+    val repository: HeroRepositoryAlt by inject()
+
+    get(path = "/boruto/heroes") {
+
+        try {
+            //this endpoint accepts a query parameter
+
+            //retrieve page query defaulting to 1 if null
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5) //{explanatory message}
+
+            //respond
+            repository.getAllHeroes(page)
+
+            call.respond( message = repository.getAllHeroes(page), status = HttpStatusCode.OK)
+        }
+
+
+        catch (e: NumberFormatException) {
+
+            /*exception in Java that occurs when an attempt is made to
+            convert a string with an incorrect format to a numeric value*/
+            call.respond(message = ApiResponse(success = false,
+                    message = "Only Numbers Allowed"),
+                    status = HttpStatusCode.BadRequest)
+
+        }
+        //catch error thrown at require(){}
+        catch (e: IllegalArgumentException) {
+
+            /* call.respond(message = ApiResponse(success = false,
+                     message = "Heroes not found"),
+                     status = HttpStatusCode.NotFound)*/
+
+            call.respond(message = ApiResponse(success = false,
+                    message = "Heroes not found"),
+                    status = HttpStatusCode.NotFound)
+
+        }
+    }
+
+
+
+}
